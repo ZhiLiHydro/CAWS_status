d72d53f2ca17c63a9c8eefc26a9e4247bc90c281
ZhiLiHydro <43716309+ZhiLiHydro@users.noreply.github.com>
2020-05-20 15:56:25 -0500

    Updated: 2020-05-20 15:56:25

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# Chicago Area Waterway System status (daily update)
+# Chicago Area Waterway System status (auto daily update)
+
+## Gage height
 
 ![gaugeheightIL](https://github.com/ZhiLiHydro/CAWS_status/blob/master/gauge-IL.png)
 
 ![gaugeheightIN](https://github.com/ZhiLiHydro/CAWS_status/blob/master/gauge-IN.png)
 
-#### Note: Data retrieval is done by the [HydroFunctions](https://github.com/mroberge/hydrofunctions) project
+## Discharge
+
+![dischargeIL](https://github.com/ZhiLiHydro/CAWS_status/blob/master/discharge-IL.png)
+
+![dischargeIN](https://github.com/ZhiLiHydro/CAWS_status/blob/master/discharge-IN.png)
+
+#### The [HydroFunctions](https://github.com/mroberge/hydrofunctions) project is applied to retrieve USGS Water Services data
 
--- hf.py
+++ hf.py
@@ -23,45 +23,87 @@ stationName = ['Lake Michigan at Chicago Lock',
         'CSSC near Lemont, IL',
         'Little Calumet River at South Holland, IL',
         'CSSC at Romeoville, IL']
-
 df = hf.NWIS(stationNum, 'iv', period='P7D', parameterCd='00065').get_data().df()
-
 for n in stationNum:
     df.rename(columns={'USGS:' + n + ':00065:00000': stationName[stationNum.index(n)]}, inplace=True)
-
 df['CSSC near Lemont, IL'] -= 27.431
 df['Little Calumet River at South Holland, IL'] -= 4.48
 df['CSSC at Romeoville, IL'] -= 27.431
-
-df.to_csv('df-IL.csv', float_format='%.2f', na_rep='nan')
+df.to_csv('gauge-IL-dataframe.csv', float_format='%.2f', na_rep='nan')
 ax = df.plot(linewidth=.5, marker='o', markersize=.5, figsize=(8,7))
 ax.grid(color='grey', linestyle=':')
 plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
 plt.ylabel('Gage height in feet (Chicago City Datum)')
 plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
 plt.tight_layout()
-plt.savefig('./gauge-IL.png', dpi=150)
+plt.savefig('gauge-IL.png', dpi=150)
 plt.close()
 
 
-stationNum = ['05536357', '04092750']
-stationName = ['Grand Calumet River at Hohman Ave at Hammond, IN', 'Indiana Harbor Canal at East Chicago, IN']
 
+
+stationNum = ['05536357', '04092750', '04092677']
+stationName = ['Grand Calumet River at Hohman Ave at Hammond, IN', 
+        'Indiana Harbor Canal at East Chicago, IN',
+        'Grand Calumet River at Industrial Hwy at Gary, IN']
 df = hf.NWIS(stationNum, 'iv', period='P7D', parameterCd='00065').get_data().df()
-
 for n in stationNum:
     df.rename(columns={'USGS:' + n + ':00065:00000': stationName[stationNum.index(n)]}, inplace=True)
-
 df['Grand Calumet River at Hohman Ave at Hammond, IN'] -= 4.48
 df['Indiana Harbor Canal at East Chicago, IN'] -= 9.28
-
-df.to_csv('df-IN.csv', float_format='%.2f', na_rep='nan')
+df['Grand Calumet River at Industrial Hwy at Gary, IN'] += 0.55
+df.to_csv('gauge-IN-dataframe.csv', float_format='%.2f', na_rep='nan')
 ax = df.plot(linewidth=.5, marker='o', markersize=.5, figsize=(8,7))
 ax.grid(color='grey', linestyle=':')
 plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
 plt.ylabel('Gage height in feet (Chicago City Datum)')
 plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
 plt.tight_layout()
-plt.savefig('./gauge-IN.png', dpi=150)
+plt.savefig('gauge-IN.png', dpi=150)
 plt.close()
+
+
+
+
+stationNum = ['05536890', '05536290', '05533600', '05537980']
+stationName = ['CSSC near Lemont, IL',
+        'Little Calumet River at South Holland, IL',
+        'Des Plaines River near Lemont, IL',
+        'Des Plaines River at Route 53 at Joliet, IL']
+df = hf.NWIS(stationNum, 'iv', period='P7D', parameterCd='00060').get_data().df()
+for n in stationNum:
+    df.rename(columns={'USGS:' + n + ':00060:00000': stationName[stationNum.index(n)]}, inplace=True)
+df.to_csv('discharge-IL-dataframe.csv', float_format='%.2f', na_rep='nan')
+ax = df.plot(linewidth=.5, marker='o', markersize=.5, figsize=(8,7))
+ax.grid(color='grey', linestyle=':')
+plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
+plt.ylabel('Discharge in cubic feet per second')
+plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
+plt.tight_layout()
+plt.savefig('discharge-IL.png', dpi=150)
+plt.close()
+
+
+
+
+stationNum = ['05536357', '04092750', '04092677']
+stationName = ['Grand Calumet River at Hohman Ave at Hammond, IN',
+        'Indiana Harbor Canal at East Chicago, IN',
+        'Grand Calumet River at Industrial Hwy at Gary, IN']
+df = hf.NWIS(stationNum, 'iv', period='P7D', parameterCd='00060').get_data().df()
+for n in stationNum:
+    df.rename(columns={'USGS:' + n + ':00060:00000': stationName[stationNum.index(n)]}, inplace=True)
+df['Grand Calumet River at Hohman Ave at Hammond, IN'] -= 4.48
+df['Indiana Harbor Canal at East Chicago, IN'] -= 9.28
+df['Grand Calumet River at Industrial Hwy at Gary, IN'] += 0.459
+df.to_csv('discharge-IN-dataframe.csv', float_format='%.2f', na_rep='nan')
+ax = df.plot(linewidth=.5, marker='o', markersize=.5, figsize=(8,7))
+ax.grid(color='grey', linestyle=':')
+plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
+plt.ylabel('Discharge in cubic feet per second')
+plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
+plt.tight_layout()
+plt.savefig('discharge-IN.png', dpi=150)
+plt.close()
+
 
