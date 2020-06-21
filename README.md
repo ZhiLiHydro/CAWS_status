8c9e6e529bb822b16cf4b88ba0a5a1fd13e004b7
Zhi Li <43716309+ZhiLiHydro@users.noreply.github.com>
2020-06-21 11:01:02 -0500

    improvements

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# Chicago Area Waterway System status (auto daily update)
+# [Chicago Area Waterway System](https://mwrd.org/chicago-area-waterway-system) (CAWS) status
+#(auto daily update)
 
 ## 0. Map of station locations
 
-<div class="iframe_container">
-  <iframe src="https://www.google.com/maps/d/embed?mid=1SZREv8sLH-2QZTxpYXC1ILf0a1-GgQw9" width="640" height="480"> </iframe>
-</div>
+Check station locations on this map
+
+https://www.google.com/maps/d/embed?mid=1SZREv8sLH-2QZTxpYXC1ILf0a1-GgQw9
 
 ## 1. Gage height
 
 ### 1.1 Illinois
 
-![gaugeheightIL](https://github.com/ZhiLiHydro/CAWS_status/blob/master/gauge-IL.png)
+![gageheightIL](https://github.com/ZhiLiHydro/CAWS_status/blob/master/gageHeight-IL.png)
 
 ### 1.2 Indiana
 
-![gaugeheightIN](https://github.com/ZhiLiHydro/CAWS_status/blob/master/gauge-IN.png)
+![gageheightIN](https://github.com/ZhiLiHydro/CAWS_status/blob/master/gageHeight-IN.png)
 
 ## 2. Discharge
 
@@ -26,7 +27,7 @@
 
 ![dischargeIN](https://github.com/ZhiLiHydro/CAWS_status/blob/master/discharge-IN.png)
 
-#### Credit: The [HydroFunctions](https://github.com/mroberge/hydrofunctions) project is applied to retrieve USGS Water Data
+#### Credit: The [HydroFunctions](https://github.com/mroberge/hydrofunctions) project is applied to get water data from USGS NWIS
 
 
 
--- hf.py
+++ hf.py
@@ -1,9 +1,12 @@
+# coding: utf-8
+import os
 from datetime import datetime
 import hydrofunctions as hf
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.family'] = 'Times New Roman'
 
 stationNum = ['04087440', ## 1
         '05536121',       ## 2
@@ -31,15 +34,14 @@ for i, n in enumerate(stationNum):
         df['USGS ' + n + ': ' + stationName[stationNum.index(n)]] -= 27.431
     if 'Little Calumet' in stationName[stationNum.index(n)]:
         df['USGS ' + n + ': ' + stationName[stationNum.index(n)]] -= 4.48
-df.to_csv('gauge-IL-dataframe.csv', float_format='%.2f', na_rep='nan')
+df.to_csv('gageHeight-IL-dataframe.csv', float_format='%.2f', na_rep='nan')
 color = np.append(plt.cm.Dark2(np.linspace(0, 1, 8)), [[0, 0, 0, 1]], axis=0)
-ax = df.plot(linewidth=.75, marker='o', markersize=.75, figsize=(8,7))
-ax.grid(color='grey', linestyle=':')
+df.plot(linewidth=.75, marker='+', markersize=.75, figsize=(8,7)).grid(color='grey', linestyle=':')
 plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
 plt.ylabel('Gage height, feet (Chicago City Datum)')
 plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
 plt.tight_layout()
-plt.savefig('gauge-IL.png', dpi=150)
+plt.savefig('gageHeight-IL.png', dpi=150)
 plt.close()
 
 
@@ -59,14 +61,13 @@ for i, n in enumerate(stationNum):
         df['USGS ' + n + ': ' + stationName[stationNum.index(n)]] -= 9.28
     if 'Gary' in stationName[stationNum.index(n)]:
         df['USGS ' + n + ': ' + stationName[stationNum.index(n)]] += 0.55
-df.to_csv('gauge-IN-dataframe.csv', float_format='%.2f', na_rep='nan')
-ax = df.plot(linewidth=.75, marker='o', markersize=.75, figsize=(8,7))
-ax.grid(color='grey', linestyle=':')
+df.to_csv('gageHeight-IN-dataframe.csv', float_format='%.2f', na_rep='nan')
+df.plot(linewidth=.75, marker='+', markersize=.75, figsize=(8,5.5)).grid(color='grey', linestyle=':')
 plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
 plt.ylabel('Gage height, feet (Chicago City Datum)')
 plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
 plt.tight_layout()
-plt.savefig('gauge-IN.png', dpi=150)
+plt.savefig('gageHeight-IN.png', dpi=150)
 plt.close()
 
 
@@ -94,8 +95,7 @@ for i, n in enumerate(stationNum):
     df.rename(columns={'USGS:' + n + ':00060:00000': 'USGS ' + n + ': ' + stationName[stationNum.index(n)]}, inplace=True)
 df[df < 0] = np.nan
 df.to_csv('discharge-IL-dataframe.csv', float_format='%.2f', na_rep='nan')
-ax = df.plot(linewidth=.75, marker='o', markersize=.75, figsize=(8,7), logy=True)
-ax.grid(color='grey', linestyle=':')
+df.plot(linewidth=.75, marker='+', markersize=.75, figsize=(8,7), logy=True).grid(color='grey', linestyle=':')
 plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
 plt.ylabel('Discharge, cubic feet per second')
 plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
@@ -115,8 +115,10 @@ for i, n in enumerate(stationNum):
     df.drop(df.columns[2*(len(stationNum)-i)-1], axis=1, inplace=True)
     df.rename(columns={'USGS:' + n + ':00060:00000': 'USGS ' + n + ': ' + stationName[stationNum.index(n)]}, inplace=True)
 df.to_csv('discharge-IN-dataframe.csv', float_format='%.2f', na_rep='nan')
-ax = df.plot(linewidth=.75, marker='o', markersize=.75, figsize=(8,7))
-ax.grid(color='grey', linestyle=':')
+df.plot(y=['USGS 04092750: Indiana Harbor Canal at East Chicago, IN',
+           'USGS 04092677: Grand Calumet River at Industrial Hwy at Gary, IN',
+           'USGS 05536357: Grand Calumet River at Hohman Ave at Hammond, IN'],
+        linewidth=.75, marker='+', markersize=.75, figsize=(8,5.5)).grid(color='grey', linestyle=':')
 plt.legend(edgecolor='black', facecolor='white', framealpha=1, markerscale=8, bbox_to_anchor=(.5,-.2), loc='upper center')
 plt.ylabel('Discharge, cubic feet per second')
 plt.title('Updated at ' + datetime.now().strftime('%m/%d/%Y %H:%M:%S') + ' US Central Time')
@@ -124,4 +126,4 @@ plt.tight_layout()
 plt.savefig('discharge-IN.png', dpi=150)
 plt.close()
 
-
+os.remove('hydrofunctions_testing.log')
